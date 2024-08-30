86b7a6d3f4a5c3b5fef26a1001126d5ee650a2a5
Noah Syrkis <noah@syrkis.com>
2024-08-30 11:53:55 +0200

    water, forest and buildings

--- parabellum/geo.py
+++ parabellum/geo.py
@@ -7,28 +7,25 @@ import rasterio
 from geopy.geocoders import Nominatim
 from geopy.distance import distance
 import contextily as cx
-from contextily import Place
 import cartopy.crs as ccrs
 from jaxtyping import Array
 import osmnx as ox
 import geopandas as gpd
 from collections import namedtuple
 import jax.numpy as jnp
-import seaborn as sns
 import matplotlib.pyplot as plt
 from rasterio import features
 from typing import Tuple
-import os
 
 # %% Types
 Coords = Tuple[float, float]
 BBox = namedtuple("BBox", ["north", "south", "east", "west"])  # type: ignore
 
 # %% Constants
-provider = cx.providers.Esri.WorldImagery
-# api_key="86d0d32b-d2fe-49af-8db8-f7751f58e83f"
-# )
-# provider["url"] = provider["url"] + "?api_key={api_key}"
+provider = cx.providers.Stadia.StamenTerrainBackground(
+    api_key="86d0d32b-d2fe-49af-8db8-f7751f58e83f"
+)
+provider["url"] = provider["url"] + "?api_key={api_key}"
 
 
 # %% Coordinate function
@@ -49,22 +46,43 @@ def get_bbox(place: str, buffer: int = 1000) -> BBox:
     return BBox(north, south, east, west)
 
 
-def raster_fn(bbox: BBox, shape=(1000, 1000)) -> Array:
-    buildings = ox.features_from_bbox(bbox=bbox, tags={"building": True})
-    gdf = gpd.GeoDataFrame(buildings).set_crs("EPSG:4326").to_crs("EPSG:3857")
-    t = rasterio.transform.from_bounds(*gdf.total_bounds, shape[0], shape[1])  # type: ignore
-    raster = features.rasterize(gdf.geometry, out_shape=shape, transform=t, fill=0)  # type: ignore
-    # img, ext = cx.bounds2img(*gdf.total_bounds, source=provider)
-    return jnp.array(raster)  # , jnp.array(img)
+# def raster_fn(bbox: BBox, shape=(1000, 1000)) -> Array:
+#     buildings = ox.features_from_bbox(bbox=bbox, tags={"building": True})
+#     gdf = gpd.GeoDataFrame(buildings).set_crs("EPSG:4326").to_crs("EPSG:3857")
+#     t = rasterio.transform.from_bounds(*gdf.total_bounds, shape[0], shape[1])  # type: ignore
+#     raster = features.rasterize(gdf.geometry, out_shape=shape, transform=t, fill=0)  # type: ignore
+#     # img, ext = cx.bounds2img(*gdf.total_bounds, source=provider)
+#     return jnp.array(raster)  # , jnp.array(img)
 
 
 def basemap_fn(bbox: BBox) -> Array:
-    buildings = ox.features_from_bbox(bbox=bbox, tags={"building": True})
+    buildings = ox.features_from_bbox(
+        bbox=bbox,
+        tags={
+            "building": True,  # <- can't move through buildings
+            "water": True,  # <- can move slowly through and not shoot while in water
+            "landuse": "forest",  # <- can move through forest and hide in it
+        },
+    )
     gdf = gpd.GeoDataFrame(buildings).set_crs("EPSG:4326").to_crs("EPSG:3857")
     fig, ax = plt.subplots(figsize=(20, 20), subplot_kw={"projection": ccrs.Mercator()})
-    gdf.plot(ax=ax, color="black", alpha=0.5, edgecolor="black")
+    gdf.plot(ax=ax, color="black", alpha=1, edgecolor="black")
     cx.add_basemap(ax, crs=gdf.crs, source=provider, zoom="auto")
     return jnp.array(fig.canvas.renderer._renderer)  # type: ignore
+
+
+def geography_fn(bbox: BBox) -> Array:
+    buildings = ox.features_from_bbox(
+        bbox=bbox, tags={"building": True, "landuse": "forest"}
+    )
+    gdf = gpd.GeoDataFrame(buildings).set_crs("EPSG:4326").to_crs("EPSG:3857")
+    raster = raster_fn(gdf)
+
+
+def raster_fn(gdf, shape=(1000, 1000)) -> Array:
+    t = rasterio.transform.from_bounds(*gdf.total_bounds, shape[0], shape[1])  # type: ignore
+    raster = features.rasterize(gdf.geometry, out_shape=shape, transform=t, fill=0)  # type: ignore
+    return jnp.array(raster)
 
 
 # raster = raster_fn(bbox)
@@ -86,8 +104,9 @@ def to_platecarree(bbox: BBox) -> BBox:
 
 # %% Ludens
 # make cartopy map of bbox
-place = "caen, france"
-bbox = get_bbox(place, buffer=1000)
+# place in amazon forest
+place = "Thun, Switzerland"
+bbox = get_bbox(place, buffer=200)
 basemap = basemap_fn(bbox)
 
 
