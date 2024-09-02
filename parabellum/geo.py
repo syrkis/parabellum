b555db8d0a52236b31fedb977e29b0568cca9f10
Noah Syrkis <noah@syrkis.com>
2024-09-02 16:32:58 +0200

    prep to add basemap to terrain

--- parabellum/geo.py
+++ parabellum/geo.py
@@ -3,25 +3,30 @@
 # by: Noah Syrkis
 
 # %% Imports
+from parabellum import tps
 import rasterio
+from rasterio import features, transform
+
 from geopy.geocoders import Nominatim
 from geopy.distance import distance
+
 import contextily as cx
 import cartopy.crs as ccrs
+
+import jax.numpy as jnp
 from jaxtyping import Array
+import numpy as np
+
 from shapely import box
 import osmnx as ox
 import geopandas as gpd
-import numpy as np
+
 from collections import namedtuple
-import jax.numpy as jnp
-import matplotlib.pyplot as plt
-from parabellum import tps
-from rasterio import features, transform
-import os
-import seaborn as sns
-import jax.numpy as jnp
+
 from typing import Tuple
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
 
 # %% Types
 Coords = Tuple[float, float]
@@ -75,35 +80,26 @@ def get_bbox(place: str, buffer: int = 1000) -> BBox:
 #     return jnp.array(raster)  # , jnp.array(img)
 
 
-def basemap_fn(bbox: BBox) -> Array:
-    buildings = ox.features_from_bbox(
-        bbox=bbox,
-        tags={
-            "building": True,  # <- can't move through buildings
-            # "water": True,  # <- can move slowly through and not shoot while in water
-            # "landuse": "forest",  # <- can move through forest and hide in it
-            "highway": True,  # <- can move quickly on roads
-        },
-    )
-    gdf = gpd.GeoDataFrame(buildings).set_crs("EPSG:4326").to_crs("EPSG:3857")
+def basemap_fn(bbox: BBox, gdf) -> Array:
     fig, ax = plt.subplots(figsize=(20, 20), subplot_kw={"projection": ccrs.Mercator()})
-    gdf.plot(ax=ax, color="black", alpha=1, edgecolor="black")
-    cx.add_basemap(ax, crs=gdf.crs, source=provider, zoom="auto")
+    gdf.plot(ax=ax, color="black", alpha=1, edgecolor="black")  # type: ignore
+    cx.add_basemap(ax, crs=gdf.crs, source=provider, zoom="auto") # type: ignore
+    # remove whitespace around the image
+    ax.set_xlim(gdf.total_bounds[[0, 2]])
+    ax.set_ylim(gdf.total_bounds[[1, 3]])
+    plt.axis("off")
+    plt.tight_layout()
+    fig.canvas.draw()
     image = jnp.array(fig.canvas.renderer._renderer)  # type: ignore
-    # crop image
-
     plt.close(fig)
     return image
 
 def geography_fn(bbox: BBox):
     map_data = ox.features_from_bbox(bbox=bbox, tags=tags)
     gdf = gpd.GeoDataFrame(map_data)
-    # .clip to bbox
-    gdf = gdf.clip(box(bbox.west, bbox.south, bbox.east, bbox.north))
-    gdf = gdf.to_crs("EPSG:3857")
-    # gdf = gdf.clip(box(mercator_bbox.west, mercator_bbox.south, mercator_bbox.east, mercator_bbox.north))
+    gdf = gdf.clip(box(bbox.west, bbox.south, bbox.east, bbox.north)).to_crs("EPSG:3857")
     raster = raster_fn(gdf)
-    basemap = basemap_fn(bbox)
+    basemap = basemap_fn(bbox, gdf)
     return raster, basemap
 
 
@@ -111,7 +107,6 @@ def raster_fn(gdf, shape=(1000, 1000)) -> tps.Terrain:
     bbox = gdf.total_bounds
     t = transform.from_bounds(*bbox, *shape)  # type: ignore
     raster = jnp.array([feature_fn(t, feature, gdf) for feature in ["building", "water", "landuse"]])
-    # raster = features.rasterize(gdf.geometry, out_shape=shape, transform=t, fill=0)  # type: ignore
     terrain = tps.Terrain(land=raster[0], water=raster[1], forest=raster[2])
     return terrain
 
@@ -127,7 +122,8 @@ place = "Thun, Switzerland"
 bbox = get_bbox(place, buffer=300)
 raster, basemap = geography_fn(bbox)
 # %%
-fig, axes = plt.subplots(1, 3, figsize=(20, 20))
-axes[0].imshow(raster.land, cmap="gray")
-axes[1].imshow(raster.water, cmap="gray")
-axes[2].imshow(raster.forest, cmap="gray")
+# fig, axes = plt.subplots(1, 3, figsize=(20, 20))
+# axes[0].imshow(raster.land, cmap="gray")
+# axes[1].imshow(raster.water, cmap="gray")
+# axes[2].imshow(raster.forest, cmap="gray")
+basemap.shape, raster.land.shape
