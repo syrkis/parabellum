15fae30bc11094d35f77c576fc502507c59114a2
Noah Syrkis <noah@syrkis.com>
2024-09-02 15:48:55 +0200

    layered geo maps

--- parabellum/geo.py
+++ parabellum/geo.py
@@ -11,10 +11,15 @@ import cartopy.crs as ccrs
 from jaxtyping import Array
 import osmnx as ox
 import geopandas as gpd
+import numpy as np
 from collections import namedtuple
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
-from rasterio import features
+from rasterio import features, transform
+import os
+import seaborn as sns
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
 from typing import Tuple
 
 # %% Types
@@ -22,11 +27,25 @@ Coords = Tuple[float, float]
 BBox = namedtuple("BBox", ["north", "south", "east", "west"])  # type: ignore
 
 # %% Constants
-provider = cx.providers.Stadia.StamenTerrainBackground(
+provider = cx.providers.Stadia.StamenTerrainBackground(  # type: ignore
     api_key="86d0d32b-d2fe-49af-8db8-f7751f58e83f"
 )
 provider["url"] = provider["url"] + "?api_key={api_key}"
+tags = {"building": True, "water": True, "landuse": "forest"}  #  "road": True}
 
+
+def to_mercator(bbox: BBox) -> BBox:
+    proj = ccrs.Mercator()
+    west, south = proj.transform_point(bbox.west, bbox.south, ccrs.PlateCarree())
+    east, north = proj.transform_point(bbox.east, bbox.north, ccrs.PlateCarree())
+    return BBox(north=north, south=south, east=east, west=west)
+
+
+def to_platecarree(bbox: BBox) -> BBox:
+    proj = ccrs.PlateCarree()
+    west, south = proj.transform_point(bbox.west, bbox.south, ccrs.Mercator())
+    east, north = proj.transform_point(bbox.east, bbox.north, ccrs.Mercator())
+    return BBox(north=north, south=south, east=east, west=west)
 
 # %% Coordinate function
 def get_coordinates(place: str) -> Coords:
@@ -60,59 +79,49 @@ def basemap_fn(bbox: BBox) -> Array:
         bbox=bbox,
         tags={
             "building": True,  # <- can't move through buildings
-            "water": True,  # <- can move slowly through and not shoot while in water
+            # "water": True,  # <- can move slowly through and not shoot while in water
             "landuse": "forest",  # <- can move through forest and hide in it
+            "highway": True,  # <- can move quickly on roads
         },
     )
     gdf = gpd.GeoDataFrame(buildings).set_crs("EPSG:4326").to_crs("EPSG:3857")
     fig, ax = plt.subplots(figsize=(20, 20), subplot_kw={"projection": ccrs.Mercator()})
     gdf.plot(ax=ax, color="black", alpha=1, edgecolor="black")
     cx.add_basemap(ax, crs=gdf.crs, source=provider, zoom="auto")
-    return jnp.array(fig.canvas.renderer._renderer)  # type: ignore
+    # remove whitespace on canvas
+    fig.canvas.draw()
+    image = jnp.array(fig.canvas.renderer._renderer)  # type: ignore
+    # crop image
+    plt.close(fig)
+    return image
 
-
-def geography_fn(bbox: BBox) -> Array:
-    buildings = ox.features_from_bbox(
-        bbox=bbox, tags={"building": True, "landuse": "forest"}
-    )
+def geography_fn(bbox: BBox):
+    buildings = ox.features_from_bbox(bbox=bbox, tags=tags)
     gdf = gpd.GeoDataFrame(buildings).set_crs("EPSG:4326").to_crs("EPSG:3857")
-    raster = raster_fn(gdf)
+    raster = raster_fn(bbox, gdf)
+    basemap = basemap_fn(bbox)
+    return gdf, raster, basemap
 
 
-def raster_fn(gdf, shape=(1000, 1000)) -> Array:
-    t = rasterio.transform.from_bounds(*gdf.total_bounds, shape[0], shape[1])  # type: ignore
-    raster = features.rasterize(gdf.geometry, out_shape=shape, transform=t, fill=0)  # type: ignore
+def raster_fn(bbox, gdf, shape=(1000, 1000)) -> Array:
+    bbox = gdf.total_bounds
+    t = transform.from_bounds(*bbox, *shape)  # type: ignore
+    raster = jnp.array([feature_fn(t, feature, gdf) for feature in ["building", "water", "landuse"]])
+    # raster = features.rasterize(gdf.geometry, out_shape=shape, transform=t, fill=0)  # type: ignore
     return jnp.array(raster)
 
+def feature_fn(t, feature, gdf):
+    # subset where feature is not nan
+    gdf = gdf[~gdf[feature].isna()]
+    raster = features.rasterize(gdf.geometry, out_shape=(1000, 1000), transform=t, fill=0)  # type: ignore
+    return raster
 
-# raster = raster_fn(bbox)
-
-
-def to_mercator(bbox: BBox) -> BBox:
-    proj = ccrs.Mercator()
-    west, south = proj.transform_point(bbox.west, bbox.south, ccrs.PlateCarree())
-    east, north = proj.transform_point(bbox.east, bbox.north, ccrs.PlateCarree())
-    return BBox(north=north, south=south, east=east, west=west)
-
-
-def to_platecarree(bbox: BBox) -> BBox:
-    proj = ccrs.PlateCarree()
-    west, south = proj.transform_point(bbox.west, bbox.south, ccrs.Mercator())
-    east, north = proj.transform_point(bbox.east, bbox.north, ccrs.Mercator())
-    return BBox(north=north, south=south, east=east, west=west)
-
-
-# %% Ludens
-# make cartopy map of bbox
-# place in amazon forest
 place = "Thun, Switzerland"
 bbox = get_bbox(place, buffer=200)
-basemap = basemap_fn(bbox)
-
-
-# %% Plot
-plt.figure(figsize=(10, 10))
-plt.imshow(basemap)
-plt.tight_layout()
-plt.axis("off")
-plt.show()
+gdf, raster, basemap = geography_fn(bbox)
+# %%
+fig, axes = plt.subplots(1, 4, figsize=(20, 20))
+for i, ax in enumerate(axes[:-1]):
+    ax.imshow(raster[i], cmap="gray")
+    ax.axis("off")
+axes[-1].imshow(basemap, cmap="gray")
