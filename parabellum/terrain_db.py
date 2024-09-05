1f99230112f2cf26de718b6da22f75d213822445
Timothee-ANNE <timonathan@gmail.com>
2024-09-05 13:51:54 +0200

    make terrain from rectangles in addition to lines

--- parabellum/terrain_db.py
+++ parabellum/terrain_db.py
@@ -5,22 +5,31 @@ from parabellum import tps
 
 
 # %%
-def map_raster_map(lines, size):
-    raster = np.zeros((size, size))
-    for line in lines:
-        x0, y0, dx, dy = line
-        x0 = int(x0*size)
-        y0 = int(y0*size)
-        dx = int(dx*size)
-        dy = int(dy*size)
-        max_T = int(2**0.5 * size)
-        for t in range(max_T+1):
-            alpha = t/float(max_T)
-            x = x0 if dx == 0 else int((1 - alpha) * x0 + alpha * (x0+dx))
-            y = y0 if dy == 0 else  int((1 - alpha) * y0 + alpha * (y0+dy))
-            if 0<=x<size and 0<=y<size:
-                raster[x, y] = 1
-    return jnp.array(raster)
+def map_raster_from_line(raster, line, size):
+    x0, y0, dx, dy = line
+    x0 = int(x0*size)
+    y0 = int(y0*size)
+    dx = int(dx*size)
+    dy = int(dy*size)
+    max_T = int(2**0.5 * size)
+    for t in range(max_T+1):
+        alpha = t/float(max_T)
+        x = x0 if dx == 0 else int((1 - alpha) * x0 + alpha * (x0+dx))
+        y = y0 if dy == 0 else  int((1 - alpha) * y0 + alpha * (y0+dy))
+        if 0<=x<size and 0<=y<size:
+            raster[x, y] = 1
+    return raster
+
+
+# %%
+def map_raster_from_rect(raster, rect, size):
+    x0, y0, dx, dy = rect
+    x0 = int(x0*size)
+    y0 = int(y0*size)
+    dx = int(dx*size)
+    dy = int(dy*size)
+    raster[x0:x0+dx, y0:y0+dy] = 1
+    return raster
 
 
 # %%
@@ -31,8 +40,15 @@ empty_color = jnp.array([255, 255, 255, 255])
 
 def make_terrain(terrain_args, size):
     args = {}
-    for key, lines in terrain_args.items():
-        args[key] = map_raster_map(lines, size).T
+    for key, config in terrain_args.items():
+        raster = np.zeros((size, size))
+        if config is not None:
+            for elem in config:
+                if "line" in elem:
+                    raster = map_raster_from_line(raster, elem["line"], size)
+                elif "rect" in elem:
+                    raster = map_raster_from_rect(raster, elem["rect"], size)
+        args[key] = jnp.array(raster.T)
     basemap = jnp.where(args["building"][:,:,None], jnp.tile(building_color, (size, size, 1)), jnp.tile(empty_color, (size,size, 1)))
     basemap = jnp.where(args["water"][:,:,None], jnp.tile(water_color, (size, size, 1)), basemap)
     basemap = jnp.where(args["forest"][:,:,None], jnp.tile(forest_color, (size, size, 1)), basemap)
@@ -42,22 +58,24 @@ def make_terrain(terrain_args, size):
 
 # %%
 db = {
-    "blank": {'building': [], 'water': [], 'forest': []},
-    "F": {'building': [[0.25, 0.33, 0.5, 0], [0.75, 0.33, 0., 0.25], [0.50, 0.33, 0., 0.25]], 'water': [], 'forest': []},
+    "blank": {'building': None, 'water': None, 'forest': None},
+    "F": {'building': [{"line": [0.25, 0.33, 0.5, 0]}, {"line":[0.75, 0.33, 0., 0.25]}, {"line":[0.50, 0.33, 0., 0.25]}], 'water': None, 'forest': None},
     "stronghold": {'building': [
-    [0.2, 0.275, 0.2, 0.], [0.2, 0.275, 0.0, 0.2], 
-    [0.4, 0.275, 0.0, 0.2], [0.2, 0.475, 0.2, 0.], 
+    {"line":[0.2, 0.275, 0.2, 0.]}, {"line":[0.2, 0.275, 0.0, 0.2]}, 
+    {"line":[0.4, 0.275, 0.0, 0.2]}, {"line":[0.2, 0.475, 0.2, 0.]}, 
 
-    [0.2, 0.525, 0.2, 0.], [0.2, 0.525, 0.0, 0.2], 
-    [0.4, 0.525, 0.0, 0.2], [0.2, 0.725, 0.525, 0.], 
+    {"line":[0.2, 0.525, 0.2, 0.]}, {"line": [0.2, 0.525, 0.0, 0.2]}, 
+    {"line":[0.4, 0.525, 0.0, 0.2]}, {"line": [0.2, 0.725, 0.525, 0.]}, 
 
-    [0.75, 0.25, 0., 0.2], [0.75, 0.55, 0., 0.19],
-    [0.6, 0.25, 0.15, 0.], 
-    ], 'water': [], 'forest': []},
-    "playground": {'building': [[0.5, 0.5, 0.5, 0.]], 'water': [], 'forest': []},
-    "water_park": {'building': [[0., 0.5, 0.33, 0.]], "water": [[0.33, 0.5, 0.33, 0.]], "forest": [[0.66, 0.5, 0.33, 0.]]},
+    {"line":[0.75, 0.25, 0., 0.2]}, {"line":[0.75, 0.55, 0., 0.19]},
+    {"line":[0.6, 0.25, 0.15, 0.]}], 'water': None, 'forest': None},
+    "playground": {'building': [{"line":[0.5, 0.5, 0.5, 0.]}], 'water': None, 'forest': None},
+    "water_park": {
+        'building': [{"line":[0.5, 0.5, 0.5, 0.]}], 
+        "water": [{"rect":[0., 0.8, 0.1, 0.05]}, {"rect": [0.2, 0.8, 0.8, 0.05]}], 
+        "forest": [{"rect": [0., 0., 1., 0.2]}]
+    },
 }
-
 
 # %% [raw]
 #     lines = [
@@ -67,52 +85,31 @@ db = {
 #     raster_map = map_raster_map(lines, size)
 #     plt.imshow(np.flip(raster_map, 0))
 
+# %% [raw]
+#     
+#     size = 50
+#     raster = np.zeros((size, size))
+#     rect = [0., 0.75, 0.2, 0.1]
+#     raster = map_raster_from_rect(raster, rect, size)
+#     rect = [0.3, 0.75, 0.7, 0.1]
+#     raster = map_raster_from_rect(raster, rect, size)
+#
+#     plt.imshow(np.flip(raster, 0))
+
 # %% [markdown]
 # # Main
-
-# %%
-def compute_astar_map(mask, goal):
-    """
-    Start from goal on a grid world to compute the shortest path to the goal from any reachable cells 
-    """
-    n = len(mask)
-    neighbors = [(-1, 0),  (0, -1), (1, 0), (0, 1)]  # N, E, S, W like in Parabellum
-    current_x = [goal[0]]
-    current_y = [goal[1]]
-    cost = 0
-    directions = np.ones(mask.shape) * -1
-    costs = np.ones(mask.shape)*(n**2)
-    costs[goal] = cost
-    while len(current_x)>0:
-        cost += 1
-        new_x = np.empty(0, dtype=int)
-        new_y = np.empty(0, dtype=int)
-        
-        for n_id, (i, j) in enumerate(neighbors):
-            neighbors_x = np.array(current_x) + i
-            neighbors_y = np.array(current_y) + j
-            inside_mask = np.where(np.logical_and(np.logical_and(neighbors_x >= 0, neighbors_x < n), np.logical_and(neighbors_y >= 0, neighbors_y < n)))
-            idxs = (neighbors_x[inside_mask], neighbors_y[inside_mask])
-            valid = np.where(np.logical_and(costs[idxs] == n**2, mask[idxs]))
-            valid_idx = inside_mask[0][valid[0]]
-            idxs = (neighbors_x[valid_idx], neighbors_y[valid_idx])
-            directions[idxs] = n_id
-            costs[idxs] = cost
-            
-            new_x = np.concatenate([new_x, neighbors_x[valid_idx]])
-            new_y = np.concatenate([new_y, neighbors_y[valid_idx]])
-                    
-        current_x, current_y = new_x, new_y
-    return np.array(directions, dtype=int), np.array(costs, dtype=int)
-
 
 # %%
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     # %%
-    terrain = make_terrain(db["F"], size=100)
+    terrain = make_terrain(db["water_park"], size=50)
     plt.imshow(terrain.building)
 
     # %%
     plt.imshow(terrain.basemap)
+
+# %%
+
+# %%
