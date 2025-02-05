b09cdace9bc2f616e8e94f3d4ad98c9befbceac8
Noah Syrkis <noah@syrkis.com>
2025-02-05 12:42:40 +0100

    basic env working. switching to esch

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 # Imports ###################################################################
 from typing import Tuple
+from dataclasses import field
 
 import equinox as eqx
 
@@ -30,31 +31,31 @@ class State:
 @dataclass
 class Conf:
     place: str = "Copenhagen, Denmark"
-    knn: int = 10
     size: int = 100
+    knn: int = 5
+    num_allies: int = 10
+    num_enemies: int = 10
+    num_agents: int = 20
+    unit_types: Array = field(default_factory=lambda: jnp.zeros(20).astype(jnp.int8))
+    line_of_sight: Array = field(default_factory=lambda: jnp.array([1, 1, 1]))
+    unit_type_radiuses: Array = field(default_factory=lambda: jnp.array([1, 1, 1]))
+    unit_type_health: Array = field(default_factory=lambda: jnp.array([1, 1, 1]))
+    unit_type_attacks: Array = field(default_factory=lambda: jnp.array([1, 1, 1]))
+    unit_type_attack_ranges: Array = field(default_factory=lambda: jnp.array([1, 1, 1]))
+    unit_type_sight_ranges: Array = field(default_factory=lambda: jnp.array([1, 1, 1]))
+    unit_type_velocities: Array = field(default_factory=lambda: jnp.array([1, 1, 1]))
+    unit_type_weapon_cooldowns: Array = field(default_factory=lambda: jnp.array([1, 1, 1]))
 
 
 @dataclass
 class Env:
     cfg: Conf
-    unit_types: Array
-    num_agents: int
-    num_allies: int
-    num_enemies: int
-    line_of_sight: Array
-    unit_type_radiuses: Array
-    unit_type_health: Array
-    unit_type_attacks: Array
-    unit_type_attack_ranges: Array
-    unit_type_sight_ranges: Array
-    unit_type_velocities: Array  # distance per steps
-    unit_type_weapon_cooldowns: Array  # in number of steps
 
     def reset(self, rng: Array) -> Tuple[Obs, State]:
         return init_fn(rng, self.cfg, self)
 
     def step(self, rng, state, action) -> Tuple[Obs, State]:
-        return obs_fn(self, state), step_fn(self, state, action)
+        return obs_fn(self.cfg, state), step_fn(self, state, action)
 
 
 @dataclass
@@ -64,30 +65,30 @@ class Action:
 
 
 # %% Functions ################################################################
-@eqx.filter_jit
+# @eqx.filter_jit
 def init_fn(rng: Array, cfg: Conf, env: Env) -> Tuple[Obs, State]:  # initialize -----
-    keys, num_agents = random.split(rng), env.num_allies + env.num_enemies  # meta ----
-    types = random.choice(keys[0], jnp.arange(env.unit_type_attacks.size), (num_agents,))  #
+    keys, num_agents = random.split(rng), env.cfg.num_allies + env.cfg.num_enemies  # meta ----
+    types = random.choice(keys[0], jnp.arange(env.cfg.unit_type_attacks.size), (num_agents,))  #
     pos = random.uniform(keys[1], (num_agents, 2), minval=0, maxval=cfg.size)  # pos -
-    health = jnp.take(env.unit_type_health, types)  # health of agents by type for starting
+    health = jnp.take(env.cfg.unit_type_health, types)  # health of agents by type for starting
     state = State(unit_position=pos, unit_health=health, unit_cooldown=jnp.zeros(num_agents))  # state --
-    return obs_fn(env, state), state  # return observation and state of agents --
+    return obs_fn(cfg, state), state  # return observation and state of agents --
 
 
-@eqx.filter_jit
-def obs_fn(env: Env, state: State) -> Obs:  # return infoabout neighbors ---
+# @eqx.filter_jit
+def obs_fn(cfg: Conf, state: State) -> Obs:  # return infoabout neighbors ---
     distances = jnp.linalg.norm(state.unit_position[:, None] - state.unit_position, axis=-1)  # all dist --
-    dist, idxs = lax.approx_min_k(distances, env.cfg.knn)  # dists and idx
+    dist, idxs = lax.approx_min_k(distances, cfg.knn)  # dists and idx
     directions = jnp.take(state.unit_position, idxs, axis=0) - state.unit_position[:, None]  # direction --
-    obs = jnp.stack([idxs, dist, state.unit_health[idxs], env.unit_types[idxs]], axis=-1)  # --
-    mask = dist < env.unit_type_sight_ranges[env.unit_types][..., None]  # mask for removing hidden -
+    obs = jnp.stack([idxs, dist, state.unit_health[idxs], cfg.unit_types[idxs]], axis=-1)  # --
+    mask = dist < cfg.unit_type_sight_ranges[cfg.unit_types][..., None]  # mask for removing hidden -
     return jnp.concat([obs, directions], axis=-1) * mask[..., None]  # an observation -
 
 
 @eqx.filter_jit
 def step_fn(env: Env, state: State, action: Action) -> State:  # update agents ---
-    pos = state.unit_position + action.moving * env.unit_type_velocities[env.unit_types][..., None]
-    hp = state.unit_health + action.health * env.unit_type_attacks[env.unit_types][..., None]  #
+    pos = state.unit_position + action.moving * env.cfg.unit_type_velocities[env.cfg.unit_types][..., None]
+    hp = state.unit_health + action.health * env.cfg.unit_type_attacks[env.cfg.unit_types][..., None]  #
     return State(unit_position=pos, unit_health=hp, unit_cooldown=state.unit_cooldown)  # return -
 
 
@@ -96,22 +97,7 @@ def render_fn(env: Env, state: State) -> None:  # render the state of the agents
 
 
 # %% Main #####################################################################
-# cfg, geo = Conf(), pb.geo.geography_fn("Copenhagen, Denmark")
-# env = Env(cfg=cfg, geo=geo)
-# obs, state = env.reset(rng := random.PRNGKey(0))
-
-
-# state_seq = []
-# for _ in range(100):
-# rng, key = random.split(rng)
-# action = Action(
-# health=random.choice(key, jnp.array([0, 1]), (env.num_allies + env.num_rivals,)),
-# moving=random.uniform(key, (env.num_allies + env.num_rivals, 2), minval=-1, maxval=1),
-# )
-#
-# state = step_fn(env, state, action)
-# state_seq.append((state, action))
-# break
-#
-# with open("state.pkl", "wb") as f:
-# pickle.dump(state, f)
+env = Env(cfg=(cfg := Conf()))
+rng = random.PRNGKey(0)
+obs, state = env.reset(rng)
+print(obs, state)
