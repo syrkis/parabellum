9eb8680bbb756c42d0be708c6db12b0a67863ddc
Noah Syrkis <noah@syrkis.com>
2025-06-13 17:09:03 +0200

    jaxkd to the rescue

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 from functools import partial
 from typing import Tuple
 
-from jax import block_until_ready, jit, lax, profiler, random, tree, vmap
+from jax import jit, lax, profiler, random, tree, vmap
 from jax import numpy as jnp
 from jaxtyping import Array
 
@@ -23,38 +23,33 @@ def action_fn(cfg: Config, rng: Array) -> Action:
     return Action(pos=pos, kind=kind)
 
 
-def step_fn(cfg: Config, state: State, rng: Array) -> Tuple[State, Tuple[State, Action]]:
+def step_fn(state: State, rng: Array) -> Tuple[State, Tuple[State, Action]]:
     action = action_fn(cfg, rng)
     obs, state = env.step(cfg, rng, state, action)
     return state, (state, action)
 
 
-def traj_fn(cfg: Config, step, state, rng) -> Tuple[State, Tuple[State, Action]]:
+def traj_fn(state, rng) -> Tuple[State, Tuple[State, Action]]:
     rngs = random.split(rng, cfg.steps)
-    return lax.scan(step, state, rngs)
+    return lax.scan(step_fn, state, rngs)
 
 
 # %% Main #####################################################################
 env, cfg = pb.env.Env(), Config()
-init = vmap(jit(partial(env.init, cfg)))
-# step = partial(env.step, cfg)
 init_key, traj_key = random.split(random.PRNGKey(0), (2, cfg.sims))
 
-print("Testing with config passed directly:")
-tic = time.time()
-obs, state = init(init_key)
-state.hp.block_until_ready()
-toc = time.time()
-print(f"First call: {toc - tic:.4f} seconds")
+init = vmap(jit(partial(env.init, cfg)))
+traj = vmap(jit(traj_fn))
 
 tic = time.time()
 obs, state = init(init_key)
 state.hp.block_until_ready()
 toc = time.time()
-print(f"Second call (same): {toc - tic:.4f} seconds")
+print(f"init time: {toc - tic:.2f}s")
+
 
 tic = time.time()
-obs, state = init(traj_key)
+state, (seq, action) = traj(state, init_key)
 state.hp.block_until_ready()
 toc = time.time()
-print(f"Third call (different): {toc - tic:.4f} seconds")
+print(f"traj time: {toc - tic:.2f}s")
